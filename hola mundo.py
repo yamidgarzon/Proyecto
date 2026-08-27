ae8295b3d764c2cd0c960c0789366fd30256d538
Sala G 404 <salag404@cunadm.edu.local>
2026-08-26 20:23:21 -0500

    version 2

--- hola mundo.py
+++ hola mundo.py
@@ -1,4 +1,43 @@
-def main()
-    print ("hola mundo")
+def registrarPersonas():
 
-main() 
+# Formulario para solicitar datos
+    individuo ={}
+    
+    
+    nombre = input("Nombre completo:")
+    individuo["nombre"]=nombre
+    edad = int(input("edad:"))
+    individuo["edad"]=edad
+    ciudad = input("ciudad:")
+    individuo["ciudad"]=ciudad
+    genero=["Masculino","Femenino"]
+    generoValido=False
+    while generoValido!=True:
+        print(genero)
+        genero = input("cual su genero")
+        if genero in genero:
+            individuo["gernero"]=genero
+            generoValido=True
+    print(personas)
+    print(individuo)
+    return individuo
+
+
+def main():
+
+    cuantasPersonas=int(input( "Cuantas personas se vana a registrar?"))
+
+                        # Registrando persoanas
+    personas={}
+    for i in range (0,cuantasPersonas):
+     print("i",i)
+     print(personas)
+     personas[i]= registrarPersonas ()
+                        
+                        # Mostrar persoanas
+    cuantasPersonas(personas)  
+  
+main()
+
+
+
